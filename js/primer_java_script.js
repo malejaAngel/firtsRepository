e393476fce76655050b25620c374254a861d9e48
aleja.dra1594 <aleja.dra1594@gmail.com>
2021-09-18 15:10:20 -0500

    Clase semana 4

---
+++ js/primer_java_script.js
@@ -0,0 +1,24 @@
+//alert("primer java script en el .js");
+
+/*valiables */
+var nombre="Pepito";
+var edad=15;
+var peso=74.5;
+var masculino=true;
+var materias;
+
+/* alert sirve para mostrar informacion*/
+/*
+alert("nombre"+ nombre);
+alert("peso"+ peso);
+alert("materias"+ materias);
+*/
+
+var numero1= prompt ("ingresar numero 1");
+var numero2= prompt ("ingresar numero 2");
+var resultado= parseInt(numero1) + parseInt(numero2);
+alert("el resultado de la suma es: "+resultado);
+
+document.write("<h6>el resultado de la suma es: "+resultado+" ")
+document.write("<h1 class='display-5'> el resultado de la suma es: "+resultado+"</h1>");
+
